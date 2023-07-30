089c61af6750d8c6727c997c004d48f6aa90ab16
CardsClaw <joshr1979@gmail.com>
2023-07-29 21:29:56 -0500

    connection.js and .env debugging

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,20 @@
-//Here we will be connected to the database, able to perform
-//operations without writing new raw SQL
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-//Here is where the dotenv is utelized//IF USING React App,
-//it would already include DotEnv, *just a note*
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  // Use the JawsDB URL for the database connection when running on Heroku
+  sequelize = new Sequelize(process.env.JAWSDB_URL, {
+    dialect: 'mysql',
+  });
+} else {
+  // Use the local database credentials when running locally
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306,
-  }
-);
+  });
+}
 
-//Here we we export what we have made with sequelize to use 
 module.exports = sequelize;
