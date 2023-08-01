31fc58f31df9649f4157b04ba860b615503d02ee
CardsClaw <joshr1979@gmail.com>
2023-08-01 17:24:40 -0500

    C:/Program Files/Git/controller/index.js tested and GOOD

---
+++ controllers/api/index.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+// const incomeRoutes = require('./incomeRoutes');
+// const expensesRoutes = require('./expensesRoutes');
+// const budgetReportsRoutes = require('./budgetreportRoutes')
+
+// router.use('/incomes', incomeRoutes);
+// router.use('/expenses', expensesRoutes);
+// router.use('/budgetreports', budgetReportsRoutes);
+// // const apiRoutes = require('./apiRoutes');
+// const userRoutes = require('./user-routes'); 
+
+// // router.use('/api', apiRoutes);
+// router.use('/api/users', userRoutes); 
+
+
+
+module.exports = router;
